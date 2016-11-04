ffcab5fac0be38ad1c4839dd6584b6a0868b016b
David Shaked <ds2643@columbia.edu>
2016-11-03 20:04:01 -0400

    fixed various bugs

--- src/grepcl/core.clj
+++ src/grepcl/core.clj
@@ -1,93 +1,81 @@
-[](ns grepcl.core
+(ns grepcl.core
   (:gen-class)
-  (:require [blancas.kern.core :as kern] 
+  (:require [blancas.kern.core :as kern]
             [blancas.kern.lexer.java-style :as kern-lexer]
-            [loom.graph :as loom-graph]
-            [loom.io :as loom-io] 
-            [clojure.java.io :as java-io] 
-            [automat.viz :as automat-viz] 
+            [clojure.java.io :as java-io]
+            [automat.viz :as automat-viz]
             [automat.core :as automat]))
 
-;; grammar
-;; token  ::= any-char | n-times | zero-plus | one-plus | one-of | char | digit
-;; oneOf  ::= '[' (char (char)*)* ']'
-;; char   ::= number | digit 
-
 (defn parse-re
-    "apply kern parsing combinators to string re representation"
+    "validate regex string; output stream of chars"
     [re]
-    (let [one-of
-           (kern-lexer/brackets
-             (kern/many
-               (kern/<|>
-                 kern/letter kern/digit)))
-          zero-plus
-            (kern/sym* \*) ;; TODO should only be valid after bracket
-          one-plus
-            (kern/sym* \+) ;; TODO should only be valid after bracket
-          n-times
-            (kern-lexer/braces 
-              (kern/<*>
-                kern/digit
-                kern-lexer/comma))
-          any-char
-            (kern/sym* \.)
-          space
-            (kern/sym* \space)] ;; TODO fix space
-      (kern/value
-        (kern/many
-          (kern/<|> space     ;; TODO fix
-                    any-char 
-                    n-times   ;; TODO poor context handling
-                    zero-plus ;; TODO poor context handling
-                    one-plus  ;; TODO poor context handling
-                    one-of 
-                    ;; start
-                    ;; not
-                    ;; maybe char-?
-                    kern/letter 
-                    kern/digit)) re)))
+    	(kern/value
+          	(kern/many
+          	  (kern/<|> (kern/sym* \space) ; space
+          	            (kern/sym* \.) ; wildcard
+                            (kern/sym* \+) ; one-plus (singleton)
+                            (kern/sym* \*) ; zed-plus (singleton)
+          	            ;; zero-plus multichar ;; requires composable automata
+          	            ;; one-plus  multichar ;; requires composable automata
+          	            (kern-lexer/brackets
+          	              (kern/many kern/alpha-num)) ; selection from numbers and/or letters
+          	            kern/alpha-num)) re))
 
 (defn k->a
   "converts kern parsing output to valid automat fsm input"
   [k]
   (cond
     (nil? (first k)) []
-    ;;invalid token [temporary until error handling is added in parse-re] 
-    ;; n-times
-    ;; zero-plus
-    (and (vector? (first k))                ;; TODO treated as group
-         (= (second k) \*)) (cons
-                              (eval (cons automat/*
-                                          (flatten (first k))))
-                              (k->a (rest (rest k))))
-    ;; one-plus
+    ;; zero-plus and one-plus for sets temporarily suspended as features until composition
+    ;; of automata is found to be possible
+    ;; zero-plus (for sets)
     ;;(and (vector? (first k))
-    ;;     (= (second k) \+)) (cons
-    ;;                          (eval (cons automat/+ 
-    ;;                                (flatten (first k))))
+    ;;     (= (second k) \*)) (cons
+    ;;                          (eval (cons automat/*
+    ;;                                      (flatten (first k))))
     ;;                          (k->a (rest (rest k))))
-    (vector? (first k)) (cons 
+    ;; one plus (for sets)
+    ;;(and (vector? (first k))
+    ;;     (= (second k) \+)) (cons (cons automat/+
+    ;;                                (eval (cons automat/or (flatten (first k)))))
+    ;;                        (k->a (rest (rest k))))
+    (and (not (vector? (first k))) ;; zed-plus singleton
+         (= (second k) \*)) (cons (eval (automat/* (first k)))
+                              (k->a (rest (rest k))))
+    (and (not (vector? (first k))) ;; one-plus singleton
+         (= (second k) \+)) (cons (eval (automat/+ (first k)))
+                              (k->a (rest (rest k))))
+    (vector? (first k)) (cons  ; from set
                           (eval (cons automat/or
                                 (flatten (first k))))
                           (k->a (rest k)))
-    ;; any-char
-    (= (first k) \.) (cons
-                        (first k)
+    (= (first k) \.) (cons  ; wildcard
+                        (eval automat/any)
                         (k->a (rest k)))
-    :else (cons 
+    :else (cons
             (first k)
             (k->a (rest k)))))
 
+(defn build-automat-fsm
+  "build automat fsm"
+  [regex]
+  (-> regex 
+      parse-re 
+      k->a))
 
 (defn -main
+  "temporary -main bypasses cli interface"
   [& args]
+  (automat.viz/view (build-automat-fsm args))) ;; TODO: varify
+
+;;(defn -main
+;;  [& args]
   ;;(def abs-file-path "/home/david/Projects/temp/poop.txt")
   ;;(with-open [rdr (java-io/reader abs-file-path)]
   ;;  (let [c (kern/char-seq rdr)]
   ;;    (println (take 5 c))))
   ;;(automat-viz/view (k->a (parse-re "a[bc]d"))))
-  (k->a (parse-re "[ef]+")))
+;;  (k->a (parse-re "[ef]+")))
 
 ;;(defn parsed-re->graph-params
 ;;  "generates directed weighted graph from parsed re"
